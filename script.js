99d0334f41c4e7bce10c6b410cfc06459dbf6135
Nayak-indie <vinayakkghai@gmail.com>
2026-03-23 19:07:48 +0530

    initial

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAAjFms1hsqLmoZ5dihr3O9Wv1RRUyt9Vs",
+  authDomain: "reciptapp1.firebaseapp.com",
+  projectId: "reciptapp1",
+  storageBucket: "reciptapp1.firebasestorage.app",
+  messagingSenderId: "953106231811",
+  appId: "1:953106231811:web:45a0b24925e4ab8d2ad436"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
